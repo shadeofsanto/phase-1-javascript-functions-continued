06d9da3d4909fdc0f537edd4201732e672ab7dca
Joseph McCafferty <mcafferty.j11@gmail.com>
2023-04-20 08:47:00 -0500

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(defaultActivity = "roller-skate"){
+    return `This Saturday, I want to ${defaultActivity}!`
+}
+
+function mondayWork(workOffice = "go to the office"){
+    return `This Monday, I will ${workOffice}.`
+}
+
+function wrapAdjective(initString = "*"){
+    return function(workerString = "a hard worker") {
+        return `You are ${initString}${workerString}${initString}!`
+    }
+}
